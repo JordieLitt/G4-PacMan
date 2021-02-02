eea7175588ce93e25fcc10f773f169939e4f7c43
pwilshere <pwilshere2010@my.fit.edu>
2021-02-02 09:23:27 -0500

    Added just a bunch of stuff

--- Pacman Clone - Project Version/Assets/Scripts/EnemyController.cs
+++ Pacman Clone - Project Version/Assets/Scripts/EnemyController.cs
@@ -8,48 +8,98 @@ public class EnemyController : MonoBehaviour
     public int targetOffset;
     public bool scaredyCat;
     public GameObject waypoint;
-    public float chaseTimer;
+    public GameObject boxWaypoint;
+    private float chaseTimer;
+    public float maxChaseTimer;
+    public float maxScatterTimer;
+    public float startPercent;
+    public bool Vulnerable = false; //for when pacman eats a power pellet
+    private float vulTimer;
+    public float maxVulTimer;
     private bool chasing = true;
     private Vector3 scatterPos;
     public float timeToMove;
     private Vector3 oldPos;
     private Vector3 oldDirection = new Vector3(0,0,0);
     private Vector3 newPos;
-    
     private bool isMoving;
     private GameObject player;
     private PlayerController playerScript;
+    private int maxPellets;
+    private bool eaten = false;
+    private float eatenTimer;
+
     void Start()
     {
         player = GameObject.FindWithTag("Player");
+        if (player != null)
+            print("Found Script");
         playerScript = player.GetComponent<PlayerController>();
 
         scatterPos = waypoint.transform.position;
+        chaseTimer = maxChaseTimer;
+        maxPellets = playerScript.pelletsLeft;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        chaseTimer -= Time.deltaTime;
+
+        
+        if (Vulnerable == false)
+        {
+            chaseTimer -= Time.deltaTime;
+        } else //vulnerable is true
+        {
+            vulTimer -= Time.deltaTime;
+        }
+
         if (chaseTimer <= 0)
         {
             chasing = !chasing; //swap between chase and scatter
             oldDirection *= -1; //turn everyone around
             if (chasing)
-                chaseTimer = 20f;//reset timer
-            else
-                chaseTimer = 5f;//scatter timer is shorter as per the original
+            {
+                maxChaseTimer += 5;
+                chaseTimer = maxChaseTimer;//reset timer
+            }else
+            {
+                maxScatterTimer *= 0.9f;
+                chaseTimer = maxScatterTimer;//scatter timer is shorter as per the original
+            } 
         }
+
+        if (eaten == true && transform.position == boxWaypoint.transform.position)
+        {
+            eaten = false;
+            GetComponent<CircleCollider2D>().enabled = true;
+            //animation state change back to normal
+        }
+
+        if (Vulnerable == true && vulTimer < 0)
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
+            Vulnerable = false;
+        }
+        //print(playerScript.pelletsLeft);
+        if (playerScript.pelletsLeft > (maxPellets * startPercent)) //stops ghosts from moving until the player has eaten a certain percentage of pellets
+            return;
 
         if (isMoving) //can't change direction mid-move
             return;
         
-        if (chasing)
+        if (Mathf.Abs(transform.position.x) > 10)
+            TeleportFlip();
+        
+        if (eaten)
+        {
+            targetPos = boxWaypoint.transform.position;
+        }else if (chasing && !Vulnerable)
         {
             targetPos = player.transform.position + (playerScript.facing*targetOffset); //get the player's current location
             if (scaredyCat && Vector3.Distance(targetPos,transform.position) < 4) //run away if the scardycat gets too close
                 targetPos = scatterPos;
-        } else //scatter mode
+        } else //scatter mode or Vulnerable mode
         {
             targetPos = scatterPos;
         }
@@ -111,6 +161,29 @@ public class EnemyController : MonoBehaviour
         }
         //print(false);
         return false;
+    }
+    void TeleportFlip()
+    {
+        Vector3 temp = transform.position;
+        if (temp.x > 0)
+            temp.x -= 1;
+        else
+            temp.x += 1;
+        temp.x *= -1;
+        transform.position = temp;
+    }
+    public void VulnerableState()
+    {
+        Vulnerable = true;
+        vulTimer = maxVulTimer;
+        GetComponent<SpriteRenderer>().color = Color.blue; // change this to an animation state change
+    }
+
+    public void Eaten()
+    {
+        eaten = true;
+        GetComponent<CircleCollider2D>().enabled = true;
+        //add another animation state change here
     }
     private IEnumerator MoveEnemy(Vector3 direction)
     {
